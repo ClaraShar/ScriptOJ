209ec671bf30384cabe534722f3315ad7072b622
ClaraShar <179800587@qq.com>
2020-05-29 15:56:02 +0800

    JSX元素变量

---
+++ 3.js
@@ -0,0 +1,10 @@
+/*
+用 JSX 完成两个变量的定义：
+
+第一个变量 title 为一个具有类名为 title 的 <h1> 元素，其内容为 ScriptOJ；
+
+第二个变量 page 为一个具有类名为 content 的 <div> 元素，将之前定义的 title 变量插入其中作为它的内容。
+*/
+
+const title = <h1 className="title">ScriptOJ</h1>
+const page = <div className="content">{ title }</div>
